a148e24371569f46c260c4ac22579cfc608b6d0d
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-16 23:47:20 +0530

    [Hemant] Welcome Greetings

---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicalProgram
+{
+    public class Porogram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programs problems");
+        }
+    }
+}
