fb52ca78db8418cd98230d02e4a1c2e6f571fc9c
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-17 00:40:18 +0530

    [Hemant] Added logical Programing Problems

---
+++ LogicalProgram/StopWatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace LogicalProgram
+{
+    public class StopWatch
+    {
+        public static void stopWatch()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            //used stopwatch class to get elapsed time
+            stopwatch.Start();
+            Thread.Sleep(5000);
+            stopwatch.Stop();
+
+            Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
